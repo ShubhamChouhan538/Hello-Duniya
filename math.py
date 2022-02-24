1ebef1a9800949549c88b31da20aa3ab48b07cb3
ShubhamChouhan538 <88274056+ShubhamChouhan538@users.noreply.github.com>
2022-02-24 23:33:55 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,20 @@
+def add(x,y):
+    pass
+<<<<<<< HEAD
+  
+#subtraction header  
+=======
+   
+# subtraction commit   
+>>>>>>> parent of f59f3f8 (Initail Commit of Math.py with two other files)
+def subtract(x,y):
+    pass
+    
+def multiply(x,y):
+    pass
+    
+def divide(x,y):
+    pass
+    
+def mode(x,y):
+    pass
